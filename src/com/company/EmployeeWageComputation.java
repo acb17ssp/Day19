70f4b1859b2220dd145c8f18113aef32d5753c75
Sree Pittala <ssapittala1@sheffield.ac.uk>
2020-08-02 17:52:00 +0100

    UC-2 Completed

--- src/com/company/EmployeeWageComputation.java
+++ src/com/company/EmployeeWageComputation.java
@@ -5,17 +5,23 @@ public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome Employee"+"\n");
 
-        // Constant
+        // Constants
         int IS_FULL_TIME = 1;
+        int EMP_RATE = 20;
 
-        // Variable
+        // Variables
+        int empHours = 0;
+        int salary = 0;
         double randomNum = Math.floor(Math.random() * 10) % 2;
 
         // Selection statement
         if (randomNum == IS_FULL_TIME){
             System.out.println("Employee Present");
+            empHours = 8;
         }else{
             System.out.println("Employee Absent");
         }
+        salary = EMP_RATE * empHours;
+        System.out.println("Employee salary: " + salary);
     }
 }
