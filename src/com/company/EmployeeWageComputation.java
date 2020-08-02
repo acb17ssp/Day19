06ff686ace83611dc50cc7461f9df28aad054eca
Sree Pittala <ssapittala1@sheffield.ac.uk>
2020-08-02 18:17:31 +0100

    UC-4 Completed

--- src/com/company/EmployeeWageComputation.java
+++ src/com/company/EmployeeWageComputation.java
@@ -2,30 +2,33 @@ package com.company;
 
 public class EmployeeWageComputation {
 
+    // Constants
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int EMP_RATE = 20;
+
     public static void main(String[] args) {
         System.out.println("Welcome Employee" + "\n");
-
-        // Constants
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        int EMP_RATE = 20;
 
         // Variables
         int empHours = 0;
         int salary = 0;
 
         // random numbers (0,1,2)
-        int randomNum = (int) (Math.random() * 2 + 1);
+        int randomNum = (int)( Math.random() * 2 + 1);
 
-        // selection statement
-        if (randomNum == IS_FULL_TIME) {
-            System.out.println("Full time - Present");
-            empHours = 8;
-        } else if (randomNum == IS_PART_TIME) {
-            System.out.println("Part time - Present");
-            empHours = 4;
-        } else {
-            System.out.println("Employee is Absent");
+        // Using Switch case
+        switch(randomNum){
+            case IS_FULL_TIME:
+                System.out.println("Full time Employee");
+                empHours = 8;
+                break;
+            case IS_PART_TIME:
+                System.out.println("Part time Employee");
+                empHours = 4;
+                break;
+            default:
+                System.out.println("Absent");
         }
 
         salary = EMP_RATE * empHours;
