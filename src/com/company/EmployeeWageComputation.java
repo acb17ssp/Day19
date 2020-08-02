4222cbb23863714fe40135cfca137a8ba140a380
Sree Pittala <ssapittala1@sheffield.ac.uk>
2020-08-02 17:58:01 +0100

    UC-3 Completed

--- src/com/company/EmployeeWageComputation.java
+++ src/com/company/EmployeeWageComputation.java
@@ -3,24 +3,31 @@ package com.company;
 public class EmployeeWageComputation {
 
     public static void main(String[] args) {
-        System.out.println("Welcome Employee"+"\n");
+        System.out.println("Welcome Employee" + "\n");
 
         // Constants
         int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
         int EMP_RATE = 20;
 
         // Variables
         int empHours = 0;
         int salary = 0;
-        double randomNum = Math.floor(Math.random() * 10) % 2;
 
-        // Selection statement
-        if (randomNum == IS_FULL_TIME){
-            System.out.println("Employee Present");
+        // random numbers (0,1,2)
+        int randomNum = (int) (Math.random() * 2 + 1);
+
+        // selection statement
+        if (randomNum == IS_FULL_TIME) {
+            System.out.println("Full time - Present");
             empHours = 8;
-        }else{
-            System.out.println("Employee Absent");
+        } else if (randomNum == IS_PART_TIME) {
+            System.out.println("Part time - Present");
+            empHours = 4;
+        } else {
+            System.out.println("Employee is Absent");
         }
+
         salary = EMP_RATE * empHours;
         System.out.println("Employee salary: " + salary);
     }
